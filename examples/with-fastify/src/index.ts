8b21b7f29b85e347ba1410a6938b91c732ed0619
Keven Leone <keven.santos.sz@gmail.com>
2025-08-10 18:22:44 -0300

    Add fastify example

---
+++ examples/with-fastify/src/index.ts
@@ -0,0 +1,33 @@
+import fastify from "fastify";
+
+import LiferayOAuth2Client from "@liferay-client-extension-util/auth";
+import liferayFastify from "@liferay-client-extension-util/fastify";
+
+const PORT = Number(Bun.env.PORT || 3001);
+
+const app = fastify();
+
+app.register(liferayFastify);
+
+app.get("/", async (request, reply) => {
+    const fetchResponse = await fetch(`http://localhost:${PORT}/protected`, {
+        headers: {
+            Authorization: await LiferayOAuth2Client.getAuthorization(
+                "liferay-marketplace-etc-spring-boot-oauth-application-headless-server"
+            ),
+        },
+    });
+
+    return fetchResponse;
+});
+
+app.get("/protected", (request, reply) => ({
+    liferayAuthorization: request.liferayAuthorization,
+    message: "Accessing a protected route",
+}));
+
+app.get("/ready", async () => ({ message: "Ready" }));
+
+app.listen({ port: PORT }, () => {
+    console.log(`App running ${PORT}`);
+});
